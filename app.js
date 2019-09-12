b953dd0c564aac994ab1628412d0f7295cde3a64
pom22936 <pom22936@gmail.com>
2019-09-12 09:42:17 +0700

    update ai
    update ai

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
-const AIMLInterpreter = require('aimlinterpreter')
+const AIMLParser = require('aimlparser')
 
 const app = express()
 const port = process.env.PORT || 4000
-const aimlInterpreter = new AIMLInterpreter({ name:'node_bot'})
+const aimlParser = new AIMLParser({ name:'node_bot' })
 
-aimlInterpreter.loadAIMLFilesIntoArray(['./test-aiml.xml'])
+aimlParser.load(['./test-aiml.xml'])
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
 
 app.get('/', (req, res) => {
     res.send('Hello World')
@@ -19,7 +20,7 @@ app.get('/', (req, res) => {
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
     let msg = req.body.events[0].message.text
-    aimlInterpreter.findAnswerInLoadedAIMLFiles(msg, (answer, wildCardArray, input) => {
+    aimlParser.getResult(msg, (answer, wildCardArray, input) => {
         reply(reply_token, answer)
     })
     res.sendStatus(200)
