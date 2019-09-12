d2ce1f9e6d3b82598228c68f560cafed4ed96e52
pom22936 <pom22936@gmail.com>
2019-09-12 12:35:41 +0700

    test menu
    test menu

--- app.js
+++ app.js
@@ -26,6 +26,12 @@ app.post('/webhook', (req, res) => {
     res.sendStatus(200)
 })
 
+app.post('/bot/richmenu', (req, res) => {
+    let reply_token = req.body.events[0].replyToken
+    richmenu(reply_token)
+    res.sendStatus(200)
+})
+
 app.listen(port)
 
 function reply(reply_token, msg) {
@@ -44,6 +50,46 @@ function reply(reply_token, msg) {
 
     request.post({
         url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
+    });
+}
+
+function richmenu(reply_token) {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer 93/xB6ktp/LJ43Qisrnt9VZnlD3XLUMqcqi9HSCStI8GA56e9TBUQfjFeZBsh8J5zkgsqJJWdJrdZofv+d5eTa7pvC9f5wNyETUpj493RX+nDEK65RE3jrc4bx5/SOncN4KzqPT/mKsC0BJlkARUAwdB04t89/1O/w1cDnyilFU='
+    }
+
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        "size": {
+            "width": 2500,
+            "height": 1686
+          },
+          "selected": false,
+          "name": "Nice richmenu",
+          "chatBarText": "Tap to open",
+          "areas": [
+            {
+              "bounds": {
+                "x": 0,
+                "y": 0,
+                "width": 2500,
+                "height": 1686
+              },
+              "action": {
+                "type": "postback",
+                "data": "action=buy&itemid=123"
+              }
+            }
+          ]
+    })
+
+    request.post({
+        url: 'https://api.line.me/v2/bot/richmenu',
         headers: headers,
         body: body
     }, (err, res, body) => {
