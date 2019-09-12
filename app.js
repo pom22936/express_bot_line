005fab9c220d1d645defd57e21bec83323b3c569
pom22936 <pom22936@gmail.com>
2019-09-12 14:35:50 +0700

    test temp
    test temp

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const AIMLParser = require('aimlparser')
 
 const app = express()
 const port = process.env.PORT || 4000
-const aimlParser = new AIMLParser({ name:'node_bot' })
+const aimlParser = new AIMLParser({ name: 'node_bot' })
 
 aimlParser.load(['./test-aiml.xml'])
 
@@ -21,14 +21,8 @@ app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
     let msg = req.body.events[0].message.text
     aimlParser.getResult(msg, (answer, wildCardArray, input) => {
-        reply(reply_token, answer)
-    })
-    res.sendStatus(200)
-})
-
-app.post('/bot/richmenu', (req, res) => {
-    let reply_token = req.body.events[0].replyToken
-    richmenu(reply_token)
+            reply(reply_token, answer)
+        })
     res.sendStatus(200)
 })
 
@@ -42,54 +36,39 @@ function reply(reply_token, msg) {
 
     let body = JSON.stringify({
         replyToken: reply_token,
-        messages: [{
-            type: 'text',
-            text: msg
-        }]
+        messages: [
+            {
+                type: 'text',
+                text: msg
+            },
+            {
+                "type": "template",
+                "altText": "this is a buttons template",
+                "template": {
+                    "type": "buttons",
+                    "thumbnailImageUrl": "https://www.thesun.co.uk/wp-content/uploads/2017/03/fifa-17-2.jpg?strip=all&w=742&quality=100",
+                    "title": "Menu",
+                    "text": "Please select",
+                    "actions": [{
+                        "type": "postback",
+                        "label": "Buy",
+                        "data": "action=buy&itemid=123"
+                    }, {
+                        "type": "postback",
+                        "label": "Add to cart",
+                        "data": "action=add&itemid=123"
+                    }, {
+                        "type": "uri",
+                        "label": "View detail",
+                        "uri": "http://localhost:8080"
+                    }]
+                }
+            }
+        ]
     })
 
     request.post({
         url: 'https://api.line.me/v2/bot/message/reply',
-        headers: headers,
-        body: body
-    }, (err, res, body) => {
-        console.log('status = ' + res.statusCode);
-    });
-}
-
-function richmenu(reply_token) {
-    let headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer 93/xB6ktp/LJ43Qisrnt9VZnlD3XLUMqcqi9HSCStI8GA56e9TBUQfjFeZBsh8J5zkgsqJJWdJrdZofv+d5eTa7pvC9f5wNyETUpj493RX+nDEK65RE3jrc4bx5/SOncN4KzqPT/mKsC0BJlkARUAwdB04t89/1O/w1cDnyilFU='
-    }
-
-    let body = JSON.stringify({
-        replyToken: reply_token,
-        "size": {
-            "width": 2500,
-            "height": 1686
-          },
-          "selected": false,
-          "name": "Nice richmenu",
-          "chatBarText": "Tap to open",
-          "areas": [
-            {
-              "bounds": {
-                "x": 0,
-                "y": 0,
-                "width": 2500,
-                "height": 1686
-              },
-              "action": {
-                "type": "postback",
-                "data": "action=buy&itemid=123"
-              }
-            }
-          ]
-    })
-
-    request.post({
-        url: 'https://api.line.me/v2/bot/richmenu',
         headers: headers,
         body: body
     }, (err, res, body) => {
